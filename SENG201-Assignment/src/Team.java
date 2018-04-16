d30009a94d4912c66b4ae08c3d0639441bdbb136
Jos Craw <jcr124@uclive.ac.nz>
2018-04-16 15:37:18 +1200

    Added Team class constructor and getTeamName and getMax Size methods for team class

--- SENG201-Assignment/src/Team.java
+++ SENG201-Assignment/src/Team.java
@@ -6,7 +6,19 @@ public class Team {
 	private double teamMoney = 100;
 	private int maxSize;
 	private ArrayList<Hero> heroes = new ArrayList<Hero>();
-	
+
+	public Team(int size, String name) {
+		teamName = name;
+		maxSize = size;
+	}
+	public int getMaxSize(){
+		return maxSize;
+	}
+
+	public String getTeamName() {
+		return teamName;
+	}
+
 	public void setName(String name) {
 		teamName = name;
 	}

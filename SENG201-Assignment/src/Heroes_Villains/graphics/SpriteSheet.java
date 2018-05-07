e41d6f08231bc15c13a8bf13617409b5336b9cf4
Jos Craw <jcr124@uclive.ac.nz>
2018-05-07 13:30:43 +1200

    Fixed SpriteSheet

--- SENG201-Assignment/src/Heroes_Villains/graphics/SpriteSheet.java
+++ SENG201-Assignment/src/Heroes_Villains/graphics/SpriteSheet.java
@@ -8,7 +8,7 @@ public class SpriteSheet {
 
     public SpriteSheet(String fileName) {
         this.fileName = fileName;
-        this.sheet = ImageHandler.loadImage("/textures/" + fileName;
+        this.sheet = ImageHandler.loadImage("/textures/" + fileName);
     }
     public BufferedImage crop(int x, int y, int width, int height) {
         return sheet.getSubimage(x, y,  width, height);

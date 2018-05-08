5958d1a1b1acf8717b495832aa4f01430e17eca5
Jos Craw <jcr124@uclive.ac.nz>
2018-05-09 07:31:38 +1200

    Started animation class wrote basic constructor and a getCurrentImage method

--- SENG201-Assignment/src/Heroes_Villains/graphics/Animation.java
+++ SENG201-Assignment/src/Heroes_Villains/graphics/Animation.java
@@ -1,4 +1,17 @@
 package Heroes_Villains.graphics;
 
+import java.awt.image.BufferedImage;
+
 public class Animation {
+
+    private BufferedImage[] images;
+    private int index;
+
+    public Animation(BufferedImage[] images) {
+        this.images = images;
+    }
+
+    public BufferedImage getCurrentImage() {
+        return images[index];
+    }
 }

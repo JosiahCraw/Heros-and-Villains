cd524a0c0d040f2ba262cad62efc915a47ff189a
Jos Craw <jcr124@uclive.ac.nz>
2018-05-09 06:48:54 +1200

    Added a keyboard listener

---
+++ SENG201-Assignment/src/Heroes_Villains/Listener/KeyboardListener.java
@@ -0,0 +1,45 @@
+package Heroes_Villains.Listener;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Arrays;
+
+public class KeyboardListener implements KeyListener {
+
+    private Boolean[] keys;
+
+    public Boolean up, down, left, right;
+    public Boolean arrowUp, arrowDown, arrowLeft, arrowRight;
+
+    public KeyboardListener() {
+        keys = new Boolean[256];
+        Arrays.fill(keys, false);
+    }
+
+    public void update() {
+        up = keys[KeyEvent.VK_W];
+        left = keys[KeyEvent.VK_A];
+        down = keys[KeyEvent.VK_S];
+        right = keys[KeyEvent.VK_D];
+
+        arrowUp = keys[KeyEvent.VK_UP];
+        arrowDown = keys[KeyEvent.VK_DOWN];
+        arrowLeft = keys[KeyEvent.VK_LEFT];
+        arrowRight = keys[KeyEvent.VK_RIGHT];
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        //Unused
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        keys[e.getKeyCode()] = true;
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        keys[e.getKeyCode()] = false;
+    }
+}

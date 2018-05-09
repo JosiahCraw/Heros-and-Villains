701b977aea52211a342f3d29fa7c33bdbf2695b5
Jos Craw <jcr124@uclive.ac.nz>
2018-05-10 11:24:57 +1200

    Added innKeeper empty class

---
+++ SENG201-Assignment/src/Heroes_Villains/entities/npc/InnKeeper.java
@@ -0,0 +1,20 @@
+package Heroes_Villains.entities.npc;
+
+import java.awt.*;
+
+public class InnKeeper extends NPC {
+
+    public InnKeeper(float x, float y, String name) {
+        super(x, y, name);
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void render(Graphics graphics) {
+
+    }
+}

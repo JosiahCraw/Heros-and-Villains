d9e5ddc776ae7afe5f3637d41135ad59f476c904
Jos Craw <jcr124@uclive.ac.nz>
2018-05-11 14:24:13 +1200

    Added Cities constructor

--- SENG201-Assignment/src/Heroes_Villains/Cities/Citys.java
+++ SENG201-Assignment/src/Heroes_Villains/Cities/Citys.java
@@ -1,7 +1,15 @@
-package Heroes_Villains.Cities;
+package Heroes_Villains.cities;
+
+import Heroes_Villains.Game;
 
 public class Citys {
 
     public int noOfCities;
+    public Game game;
+    public City[] cities;
 
+    public Citys(int noOfCities, Game game) {
+        this.noOfCities = noOfCities;
+        this.game = game;
+    }
 }

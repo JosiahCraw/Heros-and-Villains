45b58f7321e4b278b8dfe84fe0dfb3eaeeed14fd
Sam Annand <sga111@uclive.ac.nz>
2018-05-18 15:22:27 +1200

    Added two new buttons, controls button and exit button

--- SENG201-Assignment/src/Heroes_Villains/States/MenuState.java
+++ SENG201-Assignment/src/Heroes_Villains/States/MenuState.java
@@ -10,15 +10,21 @@ import java.awt.*;
 public class MenuState extends State {
 
     private UIElement startButton;
+    private UIElement exitButton;
+    private UIElement controlsButton;
 
     public MenuState(Game game) {
         super(game);
         startButton = new UIButton(540, 343, game, Assets.startButton, 200, 35);
+        controlsButton = new UIButton(540, 413, game, Assets.controlsButton, 200, 35);
+        exitButton = new UIButton(540, 483, game, Assets.exitButton, 200, 35);
     }
 
     @Override
     public void update() {
         startButton.update();
+        controlsButton.update();
+        exitButton.update();
         if(game.getMouseListener().isLeftClicked() && startButton.click()){
             game.getStateHandler().setState(game.getGameState());
         }
@@ -28,6 +34,8 @@ public class MenuState extends State {
     @Override
     public void render(Graphics graphics) {
         startButton.render(graphics);
+        exitButton.render(graphics);
+        controlsButton.render(graphics);
         graphics.setFont(Assets.titleFont);
         graphics.drawString("Heroes And Villains" , 200, 200);
     }

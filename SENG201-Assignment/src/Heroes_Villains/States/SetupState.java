1a329fbbdb160849bb3b31a21a7f4c3f887cc519
Sam Annand <sga111@uclive.ac.nz>
2018-05-20 19:00:56 +1200

    More setup state stuff

--- SENG201-Assignment/src/Heroes_Villains/States/SetupState.java
+++ SENG201-Assignment/src/Heroes_Villains/States/SetupState.java
@@ -1,6 +1,8 @@
 package Heroes_Villains.States;
 
 import Heroes_Villains.Game;
+import Heroes_Villains.SystemUI.RadioButton;
+import Heroes_Villains.SystemUI.RadioButtons;
 import Heroes_Villains.SystemUI.UIButton;
 import Heroes_Villains.SystemUI.UIElement;
 import Heroes_Villains.graphics.Assets;
@@ -10,15 +12,18 @@ import java.awt.*;
 public class SetupState extends State{
 
     private UIElement backButton;
+    private UIElement citySelect;
 
     public SetupState(Game game) {
         super(game);
         backButton = new UIButton(540, 483, game, Assets.backButton, 200, 35);
+        citySelect = new RadioButtons(256, 550, game, Assets.testRadioButton, 2, 25, true, 128, 128);
     }
 
     @Override
     public void update() {
         backButton.update();
+        citySelect.update();
         if(game.getMouseListener().isLeftClicked() && backButton.click()){
             game.getMouseListener().leftClicked = false;
             game.getStateHandler().setState(game.getMenuState());
@@ -27,7 +32,11 @@ public class SetupState extends State{
 
     @Override
     public void render(Graphics graphics) {
+        graphics.drawString("Enter Team Name: " , 200, 200);
+        graphics.drawString("Pick the number of cities:" , 200, 300);
+
         backButton.render(graphics);
+        citySelect.render(graphics);
     }
     //pass
 }

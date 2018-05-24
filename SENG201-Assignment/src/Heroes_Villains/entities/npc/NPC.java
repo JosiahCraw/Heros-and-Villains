d3252bcb4af5b091b38772320d2d9d2f5ddbac62
Jos Craw <jcr124@uclive.ac.nz>
2018-05-25 10:42:17 +1200

    more jdoc

--- SENG201-Assignment/src/Heroes_Villains/entities/npc/NPC.java
+++ SENG201-Assignment/src/Heroes_Villains/entities/npc/NPC.java
@@ -2,6 +2,8 @@ package Heroes_Villains.entities.npc;
 
 import Heroes_Villains.entities.Entities;
 
+import java.awt.*;
+
 /**
  * The abstract class describing NPC entities.
  * Extends Entities.
@@ -21,4 +23,15 @@ public abstract class NPC extends Entities {
         super(x, y);
         this.name = name;
     }
+
+    /**
+     * Updates the variables beneath the NPC
+     */
+    public abstract void update();
+
+    /**
+     * Draws everything below the NPC to the graphics object.
+     * @param graphics the object everything is drawn to.
+     */
+    public abstract void render(Graphics graphics);
 }
--- SENG201-Assignment/src/Heroes_Villains/entities/npc/Villain.java
+++ SENG201-Assignment/src/Heroes_Villains/entities/npc/Villain.java
@@ -6,6 +6,10 @@ import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 
+/**
+ * Represents the villain NPC used to assess the battle state.
+ * Extends NPC.
+ */
 public class Villain extends NPC {
 
     private BufferedImage image;
@@ -13,6 +17,15 @@ public class Villain extends NPC {
     private int width, height;
     private Game game;
 
+    /**
+     * The constructor of the Villain.
+     * @param x the x position of the villain in pixels.
+     * @param y the y position of the villain in pixels.
+     * @param image the image of the villain.
+     * @param width the width of the villain.
+     * @param height the width of the villain.
+     * @param game the object containing all the objects and variables in the game.
+     */
     public Villain(float x, float y, BufferedImage image, int width, int height, Game game) {
         super(x, y, "Villain");
         this.image = image;
@@ -22,12 +35,14 @@ public class Villain extends NPC {
         bounds = new Rectangle((int) x, (int) y, width, height);
     }
 
+    @Override
     public void update() {
         if(game.getPlayer().bounds.intersects(bounds) && game.getKeyboardListener().keyJustPressed(KeyEvent.VK_F)) {
             game.getStateHandler().setState(game.getBattleState());
         }
     }
 
+    @Override
     public void render(Graphics graphics) {
         graphics.drawImage(image, (int) x, (int) y, width, height, null);
     }
